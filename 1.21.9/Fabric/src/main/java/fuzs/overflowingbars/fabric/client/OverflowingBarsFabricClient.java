2b93c7d5f16e22aa932526becc91dc0a848f4ac7
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-06 12:45:21 +0200

    release v21.9.0

--- 1.21.9/Fabric/src/main/java/fuzs/overflowingbars/fabric/client/OverflowingBarsFabricClient.java
+++ 1.21.9/Fabric/src/main/java/fuzs/overflowingbars/fabric/client/OverflowingBarsFabricClient.java
@@ -5,7 +5,6 @@ import fuzs.overflowingbars.client.OverflowingBarsClient;
 import fuzs.puzzleslib.api.client.core.v1.ClientModConstructor;
 import fuzs.puzzleslib.api.core.v1.ModLoaderEnvironment;
 import net.fabricmc.api.ClientModInitializer;
-import squeek.appleskin.api.event.HUDOverlayEvent;
 
 public class OverflowingBarsFabricClient implements ClientModInitializer {
 
@@ -19,7 +18,7 @@ public class OverflowingBarsFabricClient implements ClientModInitializer {
         if (ModLoaderEnvironment.INSTANCE.isModLoaded("appleskin")) {
             try {
                 // just disable this, it's not too useful anyway and would be annoying to get to work properly with the stacked rendering
-                HUDOverlayEvent.HealthRestored.EVENT.register(healthRestored -> healthRestored.isCanceled = true);
+//                HUDOverlayEvent.HealthRestored.EVENT.register(healthRestored -> healthRestored.isCanceled = true);
             } catch (Throwable throwable) {
                 OverflowingBars.LOGGER.warn("Failed to initialize Apple Skin integration!", throwable);
             }
